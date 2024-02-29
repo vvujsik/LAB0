d5f05eb9ee889063400321c638266ba09999e04f
Łukasz Wójcik <103131048+vvujsik@users.noreply.github.com>
2024-02-29 17:45:15 +0100

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Class1
+    {
+        int zakres;
+        public Class1(int x) { zakres = x; }
+        public void display() {
+        for(int i = 1; i <= zakres; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 fizzbuz = new Class1(15);
+            fizzbuz.display();
+        }
+    }
+}
